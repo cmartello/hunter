b5046a90ea889b1407b63217d798cedf301db26b
Chris Martello <chrismartello@gmail.com>
2010-10-09 07:48:50 -0500

    * Inventory.py is a lot better now.  Some output issues and it isn't yet set up as a form for insertions.

--- inventory.py
+++ inventory.py
@@ -1,48 +1,157 @@
 import cherrypy
+import re
 from re import search, match
 from sqlite3 import connect
 from os.path import isfile
 from hunter import Hunter
 from sys import argv
 
+
+HTML_HEAD = '''
+<HTML>
+    <HEAD>
+        <TITLE>Hunter-Inventory v0.1</TITLE>
+    </HEAD>
+    <BODY>
+        <FORM action="search" method="get">
+            <TABLE width=800 border=1>
+                <TR>
+                    <TD width=150><label for="query">Raw SQL query</LABEL>
+                    <TD><input type="text" name="query" size=100 />
+                </TR>
+            </TABLE>
+        </FORM>
+'''
+
+
+HTML_FOOT = '''
+    </BODY>
+</HTML>
+'''
+
+TABLE_HEAD = '''
+<TABLE border=1 width=800>
+    <TR>
+        <TH>#
+        <TH>Cardname
+        <TH>Cost
+        <TH>Color
+        <TH>CMC
+        <TH>Type
+        <TH>Printings
+        <TH>P/T/L
+    </TR>
+'''
+
+TABLE_ROW = '''
+    <TR>
+        <TD width=25><input name=%d type=text size=1 value=%d />
+        <TD width=150>%s
+        <TD width=75 align=center>%s
+        <TD width=100 align=center>%s
+        <TD width=25 align=center>%d
+        <TD width=150 align=center>%s
+        <TD width=150>%s
+        <TD width=* align=center>%s
+    </TR>
+'''
+# cardid, total in inventory, cardname, castcost, color, converted mana cost
+# type, printings, power/toughness or loyalty
+
+def create_inventory_db(filename):
+    inv_db = connect(filename)
+            
+    # create the main table
+    inv_db.execute('''CREATE TABLE inventory
+        (
+            entry INTEGER PRIMARY KEY AUTOINCREMENT,
+            cardid INTEGER,
+            cardname TEXT,
+            version TEXT,
+            quantity INTEGER
+        )''')
+
+    inv_db.commit()
+
+    return inv_db
+    
+
 class Inventory:
     def __init__(self, oraclefile, inventoryfile):
         # open main hunter object; it's the basis of the the entire
         # inventory system
         self.cardbase = Hunter(oraclefile, False)
-        
+        self.inventoryfile = inventoryfile
+
         # if the inventory files doesn't exist, create it.
         if not isfile(inventoryfile):
+            self.inv_db = create_inventory_db(inventoryfile)
+        else:
             self.inv_db = connect(inventoryfile)
+
+        self.inv_db.close()
+
+    def index(self):
+        '''Does no more than show a simple query page.'''
+        return HTML_HEAD + HTML_FOOT
+
+
+    def search(self, query=None):
+        '''Parses a SQL query and formats it nicely for the end user. This
+        function mandates "select * from cards where " before the query as
+        the columns are needed in a given order.
+        '''
+
+        if query is None:
+            return HTML_HEAD + "<P>Empty query!</P>" + HTML_FOOT
+
+        # first, query the hunter object
+        rows = self.cardbase.raw_query('SELECT * FROM CARDS WHERE ' + query)
+
+        # If there's no results, say so and abort.
+        if len(rows) == 0:
+            return HTML_HEAD + '<P>No results!</P>' + HTML_FOOT
+
+        # begin generating output
+        output = HTML_HEAD + TABLE_HEAD
+
+        for row in rows:
+            # pull the quantity data from inventory
+            self.inv_db = connect(self.inventoryfile)
+
+            qcheck = self.inv_db.execute(\
+                "SELECT quantity FROM inventory WHERE cardid = " + str(row[0]))
+            quantity = sum(x[0] for x in qcheck)
             
-            # create the main table
-            self.inv_db.execute('''CREATE TABLE inventory
-                (
-                    entry INTEGER PRIMARY KEY AUTOINCREMENT,
-                    cardname TEXT,
-                    version TEXT,
-                    quantity INTEGER
-                )''')
+            # default value for "power/toughness or loyalty
+            ptl = 'N/A'
 
-            # get the file format info from the cardbase
-            schema, fbase = \
-                cardbase.execute("select * from format").fetchall()[0]
+            # if the card is a creature, store its power/toughness for output
+            if search('creature', row[6], re.I) is not None:
+                ptl = row[7] + '/' +row[8]
 
-            # create the format table and write the file format info to it
-            self.inv_db.execute('''CREATE TABLE format
-                (
-                    schema INTEGER,
-                    basefile TEXT
-                ) ''')
+            # if the card is a planeswalker, store its loyalty for output
+            if search('planeswalker', row[6], re.I) is not None:
+                ptl = row[5]
 
-            self.inv_db.execute('''INSERT INTO format VALUES ('''+\
-                str(schema) + ', "' fbase + '")'
+            # add the formatted row to the output
+            output += (TABLE_ROW % (row[0], quantity, row[1], row[2], row[3], row[4], row[6], row[11], ptl))
+        
+        # add the footer
+        output += HTML_FOOT
+        
+        # close the DB
+        self.inv_db.close()
 
+        return output
 
+    index.exposed = True
+    search.exposed = True
 
 if __name__ == '__main__':
     if len(argv) != 3:
         print 'Usage: inventory.py oracle.db yourinventory.db'
         exit(1)
 
-    # if the inventory file doesn't exist, it'll have to be created
+    cherrypy.root = Inventory(argv[1], argv[2])
+    target = cherrypy.server.start()

ea4189844c89d25d05d749119b295dee580f9d10
Chris Martello <chrismartello@gmail.com>
2012-04-28 02:06:10 -0500

    * CREATE INDEX now only happens if it hasn't before.

--- mknums.py
+++ mknums.py
@@ -3,14 +3,14 @@ from hunter import Hunter
 if __name__ == '__main__':
     db = Hunter('All Sets-2012-02-05.db')
 
-    db.dbase.execute('CREATE INDEX cnames ON cards (cardname);')
-    db.dbase.execute('CREATE INDEX pubexp ON published (expansion);')
-    db.dbase.execute('CREATE INDEX pubnames ON published (name);')
+    db.dbase.execute('CREATE INDEX IF NOT EXISTS cnames ON cards (cardname);')
+    db.dbase.execute('CREATE INDEX IF NOT EXISTS pubexp ON published (expansion);')
+    db.dbase.execute('CREATE INDEX IF NOT EXISTS pubnames ON published (name);')
 
     setlist = db.query('SELECT abbreviation FROM sets ORDER BY released')
 
     for exp in setlist:
-        cardlist = db.dbase.execute('SELECT name FROM published JOIN cards ON cards.cardname = published.name WHERE published.expansion = ? AND cards.virtual = ? ORDER BY cards.cn_position, published.name', (exp[0], 'No'))
+        cardlist = db.dbase.execute('SELECT published.name FROM published JOIN cards ON cards.cardname = published.name WHERE published.expansion = ? AND cards.virtual = ? ORDER BY cards.cn_position, published.name', (exp[0], 'No'))
 
         y = 1
         for x in cardlist:
